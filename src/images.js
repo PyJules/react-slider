d557d8895df8526694d56bbc82c563283fce7683
Jules ADONSI <56733695+PyJules@users.noreply.github.com>
2020-05-10 19:18:11 +0100

    Add files via upload

---
+++ src/images.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        src : 'https://cdn.pixabay.com/photo/2017/02/26/02/37/girl-2099359_960_720.jpg',
+        text : 'La présentation de l’emplacement où se trouvera l’entreprise est également nécessaire'
+    },
+
+    {
+        src : 'https://cdn.pixabay.com/photo/2015/03/17/14/05/sparkler-677774_960_720.jpg',
+        text : 'caractéristiques du bail ou de l’achat, aménagements à prévoir, justification du lieu choisi'
+    },
+    {
+        src : 'https://cdn.pixabay.com/photo/2016/03/23/04/01/beautiful-1274056_960_720.jpg',
+        text : 'Si la future activité implique des partenariats importants, tels que des contrats',
+
+    }, 
+
+    {
+        src : 'https://cdn.pixabay.com/photo/2020/05/07/21/06/mothers-day-5143211_960_720.jpg',
+        text : 'Enfin, votre business plan doit expliquer le montage juridique du projet'
+    },
+
+    {
+        src : 'https://cdn.pixabay.com/photo/2020/05/04/20/10/read-5130581_960_720.jpg',
+        text : 'Enfin, votre business plan doit expliquer le montage juridique du projet'
+    }
+];
